bdf40efaba91251295dd2f2395f300a3e4491c6e
mostafa-abokhadra <mvxizk@gmail.com>
2024-10-08 18:04:14 +0300

    removing unneccessary files

--- server/test.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// const crypto = require('crypto');
-const bcrypt = require("bcrypt")
-// const secret = crypto.randomBytes(32).toString('hex');
-// console.log(secret);
-bcrypt.hash("okokokok", 10).then(res=>{console.log(res)});
-// console.log(__dirname + "/src");
