00bd3fe0eaabcd784c078f667e6f2e2202f82b39
Mo2men-dev <momensameh5@gmail.com>
2024-10-08 23:37:36 +0300

    refactor: added Redux and configured store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './features/app/store'
+import { increment, decrement } from './features/counter/Counter';
 
 function App() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
 
-  return (
-    <>Hello World!</>
+    return (
+    <>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <h1>Counter: {count}</h1>
+        <button onClick={() => dispatch(increment())}>+</button>
+    </>
   )
 }
 
