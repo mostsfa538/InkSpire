dceb5fe7a541b58e86bdf8eede72479f6386210d
Mo2men-dev <momensameh5@gmail.com>
2024-10-08 23:02:38 +0300

    chore: Enable CORS for cross-origin requests and configure environment variables

--- server/server.js
+++ server/server.js
@@ -5,11 +5,16 @@ const loginRoute = require('./src/routes/loginRoutes.js');
 const signupRoute = require('./src/routes/signupRoutes.js')
 const session = require('./src/middlewares/session.js');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const userRoute = require('./src/routes/userRoutes.js');
 const app = express();
 
-app.use(cors());
+dotenv.config()
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session)
--- server/src/middlewares/session.js
+++ server/src/middlewares/session.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const session = require("express-session")
 
 module.exports = session({
