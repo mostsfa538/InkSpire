54624cb51fb777ea47a1b0cba4f5099c767d0c8a
Mo2men-dev <momensameh5@gmail.com>
2024-10-10 23:21:13 +0300

    chore: update meta tags and styles in index.html, index.css, and tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#fee3c3",
+                secondary: "#83855e",
+                tertiary: "#614941",
+                success: {
+                    background: "#DFF0D8",
+                    text: "#3C763D",
+                    border: "#D6E9C6",
+                },
+                error: {
+                    background: "#F2DEDE",
+                    text: "#A94442",
+                    border: "#EBCCD1",
+                },
+                info: {
+                    background: "#D9EDF7",
+                    text: "#31708F",
+                    border: "#BCE8F1",
+                },
+            },
+            backgroundImage: {
+                'sign-in-hero': "url('/signin-hero.jpg')",
+            }
+        },
     },
     plugins: [],
 };
