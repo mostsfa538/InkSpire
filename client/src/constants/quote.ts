ce7c920f4158ded5c7c94f7b2023087f332d7a38
AbdullahOsama-ao <abdullahosama.aoao@gmail.com>
2024-10-12 01:43:28 +0300

    adding more quotes

--- client/src/constants/quote.ts
+++ client/src/constants/quote.ts
@@ -1,26 +1,50 @@
 export const quotes = [
     {
         quote: "If there's a book that you want to read, but it hasn't been written yet, then you must write it.",
-        author: "Toni Morrison"
+        author: "Toni Morrison",
     },
     {
         quote: "If the book is true, it will find an audience that is meant to read it.",
-        author: "Wally Lamb"
+        author: "Wally Lamb",
     },
     {
         quote: "A room without books is like a body without a soul.",
-        author: "Marcus Tullius Cicero"
+        author: "Marcus Tullius Cicero",
     },
     {
         quote: "Books are a uniquely portable magic.",
-        author: "Stephen King"
+        author: "Stephen King",
     },
     {
         quote: "A reader lives a thousand lives before he dies.",
-        author: "George R.R. Martin"
+        author: "George R.R. Martin",
     },
     {
         quote: "A book is a dream that you hold in your hand.",
-        author: "Neil Gaiman"
+        author: "Neil Gaiman",
     },
-]
+    {
+        quote: "The only thing you absolutely have to know is the location of the library.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "So many books, so little time.",
+        author: "Frank Zappa",
+    },
+    {
+        quote: "A book is a gift you can open again and again.",
+        author: "Garrison Keillor",
+    },
+    {
+        quote: "Books are the mirrors of the soul.",
+        author: "Virginia Woolf",
+    },
+    {
+        quote: "There is no friend as loyal as a book.",
+        author: "Ernest Hemingway",
+    },
+    {
+        quote: "Books are the plane, and the train, and the road. They are the destination, and the journey. They are home.",
+        author: "Anna Quindlen",
+    },
+];
