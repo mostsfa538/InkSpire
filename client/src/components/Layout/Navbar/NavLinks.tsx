cd0d2b044fb9fa84f5e62ede776110ca9d41fd5c
Mo2men-dev <momensameh5@gmail.com>
2024-10-15 18:22:59 +0300

    refactor: added SideMenu and update Navbar to include it

--- client/src/components/Layout/Navbar/NavLinks.tsx
+++ client/src/components/Layout/Navbar/NavLinks.tsx
@@ -1,15 +1,11 @@
 import CustomLink from "../../UI/CustomLink"
 
 function NavLinks({ flexFlow = 'flex-row' }: { flexFlow?: string }) {
+    const sideMenuStyling = flexFlow === 'flex-col' ? 'w-full bg-slate-100 p-2 rounded-md' : ''
     return (
-        <ul className={`flex ${flexFlow} gap-x-4 gap-y-2`}>
-            <li>
-                <CustomLink to='/'>Home</CustomLink>
-            </li>
-            <li>
-                <CustomLink to='/'>Home</CustomLink>
-            </li>
-        </ul>
+        <div className={`flex ${flexFlow} gap-x-4 gap-y-2`}>
+            <CustomLink to='/' styles={sideMenuStyling}>Home</CustomLink>
+        </div>
     )
 }
 
--- client/src/components/Layout/Navbar/Navbar.tsx
+++ client/src/components/Layout/Navbar/Navbar.tsx
@@ -8,10 +8,11 @@ import { IoIosMenu, IoMdClose } from 'react-icons/io';
 import Button from '../../UI/Button';
 import NavLinks from './NavLinks';
 import CustomLink from '../../UI/CustomLink';
+import SideMenu from './SideMenu';
 
 function Navbar() {
     const { user, logout } = useAuth();
-    const [sideMenu, setSideMenu] = useState(false);
+    const [displaySideMenu, setDisplaySideMenu] = useState(false);
 
     return (
         <>
@@ -21,17 +22,10 @@ function Navbar() {
                         <NavLinks />
                     </div>
                     <div className="md:hidden">
-                        <div onClick={() => setSideMenu(!sideMenu)}>
-                            {!sideMenu ? <IoIosMenu /> : <IoMdClose />}
+                        <div onClick={() => setDisplaySideMenu(!displaySideMenu)}>
+                            {!displaySideMenu ? <IoIosMenu /> : <IoMdClose />}
                         </div>
-                        <div className={`flex flex-col absolute gap-2 top-full rounded-r-md bg-secondary text-white w-fit p-2 ${sideMenu ? 'left-0' : 'left-[-1000px]'} text-xs transition-all md:hidden`}>
-                            <div className='flex text-sm font-semibold items-center gap-x-5'>
-                                <h1>Inkspire</h1>
-                                {!user && <BiShoppingBag size={15} />}
-                            </div>
-                            <hr />
-                            <NavLinks flexFlow='flex-col' />
-                        </div>
+                        <SideMenu displaySideMenu={displaySideMenu} />
                     </div>
                 </div>
                 <div className="flex-1 flex w-full justify-center">
@@ -39,10 +33,9 @@ function Navbar() {
                         <img className="w-14 h-14" src="/logo.png" alt="" />
                     </CustomLink>
                 </div>
-                <div className="flex flex-1 items-center justify-end">
-                        <div className="flex items-center max-lg:hidden">
+                <div className="flex flex-1 items-center justify-end gap-4">
+                        <div className="flex items-center max-md:hidden">
                             {user && <BiShoppingBag className="text-tertiary" size={20} />}
-                            <div className="bg-tertiary mx-10 rounded-full w-3 h-3"></div>
                         </div>
                     {!user ? (
                         <div className="flex gap-4 text-nowrap max-md:gap-2 max-lg:text-xs">
