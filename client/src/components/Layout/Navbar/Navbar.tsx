05e3d78b11d4d231083997bcbdd28763ceb340ce
AbdullahOsama-ao <abdullahosama.aoao@gmail.com>
2024-10-17 17:11:12 +0300

    enhance home interface

--- client/src/components/Layout/Navbar/Navbar.tsx
+++ client/src/components/Layout/Navbar/Navbar.tsx
@@ -1,10 +1,9 @@
-import useAuth from '../../../hooks/useAuth';
+import useAuth from "../../../hooks/useAuth";
 
-import Carts from '../Cart/Carts';
-import NavLinks from './NavLinks';
-import SideMenu from './SideMenu';
-import CustomLink from '../../UI/CustomLink';
-
+import Carts from "../Cart/Carts";
+import NavLinks from "./NavLinks";
+import SideMenu from "./SideMenu";
+import CustomLink from "../../UI/CustomLink";
 
 function Navbar() {
     const { user, logout } = useAuth();
@@ -19,27 +18,34 @@ function Navbar() {
                     <SideMenu />
                 </div>
             </div>
-            <div className="flex-1 flex w-full justify-center">
-                <CustomLink to="/">
-                    <img className="w-14 h-14" src="/logo.png" alt="" />
-                </CustomLink>
-            </div>
+            <div className="flex-1 flex w-full justify-center"></div>
             <div className="flex flex-1 items-center justify-end gap-4">
                 <div className="flex justify-end">
                     {user && <Carts carts={user.carts} />}
                 </div>
-            {!user ? (
-                <div className="flex gap-4 text-nowrap max-md:gap-2 max-lg:text-xs">
-                    <CustomLink to='/signin' button styles='border border-tertiary hover:bg-white'>Sign In</CustomLink>
-                </div>
-                ) : 
-                <div className='text-nowrap'>
-                    <CustomLink to='/' button styles='bg-secondary text-white hover:bg-tertiary' onClick={logout}>Sign Out</CustomLink>
-                </div>
-            }
+                {!user ? (
+                    <div className="flex gap-4 text-nowrap max-md:gap-2 max-lg:text-xs">
+                        <CustomLink
+                            to="/signin"
+                            button
+                            styles="border border-tertiary hover:bg-white">
+                            Sign In
+                        </CustomLink>
+                    </div>
+                ) : (
+                    <div className="text-nowrap">
+                        <CustomLink
+                            to="/"
+                            button
+                            styles="bg-secondary text-white hover:bg-tertiary"
+                            onClick={logout}>
+                            Sign Out
+                        </CustomLink>
+                    </div>
+                )}
             </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,36 +1,57 @@
-import { BiSearch } from "react-icons/bi"
-import Button from "../components/UI/Button"
-import Input from "../components/UI/Input"
-import Quote from "../components/Misc/Quote"
+import { BiSearch } from "react-icons/bi";
+import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
+import Quote from "../components/Misc/Quote";
 
 function HomePage() {
-    const voidFunc = () => {}
+    const voidFunc = () => {};
     return (
-        <div className="h-full flex flex-col justify-center items-center text-black">
-            <div className="flex-1 w-full flex justify-center items-center">
+        <div className="relative h-full flex flex-col justify-start items-center text-black">
+            <div className="fixed inset-0 z-0 opacity-10">
+                <img
+                    src="logo.png"
+                    alt="Background Logo"
+                    className="w-100 h-100 object-cover mx-auto my-auto"
+                    style={{ marginTop: "-20px" }}
+                />
+            </div>
+            <div className="relative w-full flex flex-col items-center z-10 gap-4 mt-8">
                 <h1 className="text-4xl font-bold max-md:text-2xl">
                     <span className="animate-fadeIn opacity-0">Find</span>{" "}
                     <span className="animate-fadeInDelay opacity-0">Your</span>{" "}
                     <span className="animate-blurIn opacity-0">
                         <span className="bg-flower-pattern transition-all font-extrabold bg-[length:200%_auto] animate-animateBackground bg-clip-text text-transparent">
-                            In<span className="text-black italic">k</span>spiration
+                            <span className="text-black">I</span>
+                            <span>nk</span>
+                            <span className="text-black">S</span>
+                            piration
                         </span>
                     </span>
                 </h1>
-            </div>
-            <div className="flex-1 w-full flex flex-col justify-between items-center max-md:px-4">    
                 <Quote />
                 <div className="flex bg-white p-2 gap-2 w-1/2 rounded-xl shadow-md font-semibold max-md:w-full">
-                    <span className="text-2xl flex justify-center items-center py-2 px-4 border-r-2 text-gray-400 max-md:text-lg max-md:p-2"><BiSearch /></span>
-                    <Input type="text" defaultValue="" placeHolder="Search" onChange={voidFunc} styles="w-full p-2 outline-none" />
-                    <Button text="Search" onClick={voidFunc} styles="py-2 px-8 m-auto max-md:p-2 h-fit max-md:text-xs" />
+                    <span className="text-2xl flex justify-center items-center py-2 px-4 border-r-2 text-gray-400 max-md:text-lg max-md:p-2">
+                        <BiSearch />
+                    </span>
+                    <Input
+                        type="text"
+                        defaultValue=""
+                        placeHolder="Search"
+                        onChange={voidFunc}
+                        styles="w-full p-2 outline-none"
+                    />
+                    <Button
+                        text="Search"
+                        onClick={voidFunc}
+                        styles="py-2 px-8 m-auto max-md:p-2 h-fit max-md:text-xs"
+                    />
                 </div>
             </div>
-            <div className="flex-[2] w-full flex justify-center items-center">
+            <div className="relative flex-1 w-full flex justify-center items-center z-10">
                 TODO: Popular...
             </div>
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
