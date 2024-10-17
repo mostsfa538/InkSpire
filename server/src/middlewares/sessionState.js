2480721d6e8f2464ae20bcce4ad10492238142da
Mo2men-dev <momensameh5@gmail.com>
2024-10-17 18:07:34 +0300

    refactor: sessionState middleware and loginRoutes

--- server/src/middlewares/sessionState.js
+++ server/src/middlewares/sessionState.js
@@ -2,9 +2,9 @@ const checkSession = (req, res, next) => {
     if (!req.session.user) {
         return res.status(401).json({ message: "Unauthorized access" });
     }
-    if (req.session.user.id !== parseInt(req.params.user_id) && req.path !== '/login') {
-        return res.status(401).json({"message": "no match between current user and userId"})
-    }
+    // if (req.session.user.id !== parseInt(req.params.user_id) && req.path !== '/login') {
+    //     return res.status(401).json({"message": "no match between current user and userId"})
+    // }
     next();
 };
 
